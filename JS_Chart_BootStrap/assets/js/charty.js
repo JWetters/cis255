0f04f586e9b272da88dc7d56a5cbaeaedb14b818
JWetters <jimmy.wetters@gmail.com>
2017-10-15 22:47:59 -0400

    Add files via upload

---
+++ JS_Chart_BootStrap/assets/js/charty.js
@@ -0,0 +1,33 @@
+// JavaScript source code
+const CHART = document.getElementById("lineChart");
+console.log(CHART);
+let lineChart = new Chart(CHART, {
+    type: 'line',
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "Website Traffic",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba((75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(75,192,192,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 75, 82, 100],
+            }
+        ]
+        
+    }
+});
